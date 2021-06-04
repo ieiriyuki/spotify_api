6bae7678548e1c517f7eb344db5374cbf8c22aa5
ieiriyuki <ieiriyuki@sur.bbiq.jp>
2021-06-04 13:26:35 +0900

    Clean format codes by black

--- src/auth.py
+++ src/auth.py
@@ -7,24 +7,23 @@ import requests
 
 
 def get_client_credentials():
-    """https://developer.spotify.com/documentation/general/guides/authorization-guide/#client-credentials-flow
-    """
+    """https://developer.spotify.com/documentation/general/guides/authorization-guide/#client-credentials-flow"""
 
-    client_id = os.environ['spotify_id']
-    client_secret = os.environ['spotify_secret']
-    base_url = 'https://accounts.spotify.com/api/token'
+    client_id = os.environ["spotify_id"]
+    client_secret = os.environ["spotify_secret"]
+    base_url = "https://accounts.spotify.com/api/token"
 
-    encoded = b64encode('{}:{}'.format(client_id, client_secret).encode())
-    auth_value = 'Basic ' + encoded.decode()
+    encoded = b64encode("{}:{}".format(client_id, client_secret).encode())
+    auth_value = "Basic " + encoded.decode()
     headers = {
-        'Authorization': auth_value,
-        'Content-Type': 'application/x-www-form-urlencoded'
+        "Authorization": auth_value,
+        "Content-Type": "application/x-www-form-urlencoded",
     }
-    params = {'grant_type': 'client_credentials'}
+    params = {"grant_type": "client_credentials"}
     r = requests.post(base_url, headers=headers, params=params)
 
     return r.json()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     get_client_credentials()
--- src/search.py
+++ src/search.py
@@ -9,31 +9,27 @@ from auth import get_client_credentials
 
 
 def main():
-    base_url ='https://api.spotify.com/v1/search'
+    base_url = "https://api.spotify.com/v1/search"
     token = get_client_credentials()
 
     headers = {
-        'Authorization': '{} {}'.format(token['token_type'], token['access_token'])
+        "Authorization": "{} {}".format(token["token_type"], token["access_token"])
     }
-    params = {
-        'q': 'BUMP',  # 検索するクエリ
-        'type': 'artist',  # アーティストだけ取得する
-        'limit': 10
-    }
+    params = {"q": "BUMP", "type": "artist", "limit": 10}  # 検索するクエリ  # アーティストだけ取得する
     r = requests.get(base_url, headers=headers, params=params)
     resp = r.json()
     print(json.dumps(resp, indent=2))
 
     records = []
-    for item in resp['artists']['items']:
+    for item in resp["artists"]["items"]:
         temp = {
-            'id': item['id'],
-            'name': item['name'],
-            'type': item['type'],
-            'genres': item['genres'],
-            'popularity': item['popularity'],
-            'followers_total': item['followers']['total'],
-            'spotify_url': item['external_urls']['spotify']
+            "id": item["id"],
+            "name": item["name"],
+            "type": item["type"],
+            "genres": item["genres"],
+            "popularity": item["popularity"],
+            "followers_total": item["followers"]["total"],
+            "spotify_url": item["external_urls"]["spotify"],
         }
         records.append(temp)
 
@@ -43,5 +39,5 @@ def main():
     return 1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
